785bad91714b9e0546ad5b28fdf8b4ea21c301f8
AlibekNematov <85082765+AlibekNematov@users.noreply.github.com>
2021-10-16 08:06:33 +0300

    Add files via upload

---
+++ company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main extends MyString{
+    public static void main(String[] args) {
+        MyString str = new MyString(" He  llo");
+        MyString str2 = new MyString("World");
+        System.out.println(str.trim());
+    }
+}
