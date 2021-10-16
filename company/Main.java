96bc5b7f6a936276d645df28614e3a7736000680
AlibekNematov <85082765+AlibekNematov@users.noreply.github.com>
2021-10-16 08:06:18 +0300

    Delete company directory

--- company/Main.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Main extends MyString{
-    public static void main(String[] args) {
-        MyString str = new MyString("   He  llo   ");
-        MyString str2 = new MyString("World");
-        System.out.println(str.trim());
-    }
-}
